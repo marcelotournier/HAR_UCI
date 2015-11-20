5505d0b65d4ad34ee03ece7bfee61f83096bc4c4
Marcelo Tournier <dr.tournier@gmail.com>
2015-11-20 01:32:15 -0200

    final

---
+++ README.md
@@ -0,0 +1,24 @@
+---
+title: "Readme"
+output: html_document
+---
+
+## UCI HAR Data Code Book
+
+This dataset represents data collected from the accelerometers from the Samsung Galaxy S smartphone. 
+
+A full description is available at the site where the data was obtained: 
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+Here are the data for the project: 
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+
+There are two datasets here:
+
+1) "uci" - Merges the original training and the test sets to create one data set, extracting only the measurements on the mean and standard deviation for each measurement.  
+
+2) "uci_means" - From the data set "ici", a second data set with the average of each variable for each activity and each subject was created.
---
+++ run_analysis.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(data.table)
+
+# preparing observations from group 'test'
+subject_test=read.fwf("subject_test.txt", header=FALSE, widths = 2, sep ='\n' ,col.names = "subject")
+subject_test$Group = 'test'
+Y_test=read.fwf("y_test.txt", header=FALSE, widths = 1, sep ='\n' ,col.names = "activity")
+featurelist=read.fwf("features.txt", header=FALSE, widths = c(3,600), sep ="")
+x_test=fread('X_test.txt', sep = " ",sep2 = "\n")
+names(x_test)=as.character(featurelist$V2)
+x_test_mean=select(x_test, contains("mean()"),contains("-std()"))
+test_table=cbind(subject_test,Y_test,x_test_mean)
+
+# preparing observations from group 'train'
+subject_train=read.fwf("subject_train.txt", header=FALSE, widths = 2, sep ='\n' ,col.names = "subject")
+subject_train$Group = 'train'
+Y_train=read.fwf("y_train.txt", header=FALSE, widths = 1, sep ='\n' ,col.names = "activity")
+x_train=fread('X_train.txt', sep = " ",sep2 = "\n")
+names(x_train)=as.character(featurelist$V2)
+x_train_mean=select(x_train, contains("mean()"),contains("-std()"))
+train_table=cbind(subject_train,Y_train,x_train_mean)
+
+# Joining subsets from "test" and "train" groups:
+uci=rbind(test_table,train_table)
+
+# Renaming activity labels:
+
+uci$activity=gsub ("1","WALKING", uci$activity)
+uci$activity=gsub ("2","WALKING_UPSTAIRS", uci$activity)
+uci$activity=gsub ("3","WALKING_DOWNSTAIRS", uci$activity)
+uci$activity=gsub ("4","SITTING", uci$activity)
+uci$activity=gsub ("5","STANDING", uci$activity)
+uci$activity=gsub ("6","LAYING", uci$activity)
+
+#final touch (step 4)...
+
+uci=arrange(uci,subject)
+
+# Step 5 - From the data set in step 4, creates a second, independent tidy data set
+# with the average of each variable for each activity and each subject.
+uci_means=select(uci, contains("subject"),contains("Group"),contains("activity"), contains("mean()"))
+uci_means=ddply(uci_means, .(subject,Group,activity), numcolwise(mean))
+names(uci_means)=gsub("-mean()","",names(uci_means))
